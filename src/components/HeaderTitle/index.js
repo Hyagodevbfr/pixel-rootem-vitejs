90c6b521b8fa8397814886cccc7298f2f3a177ed
Hyago Brito <110845341+Hyagodevbfr@users.noreply.github.com>
2022-09-29 10:15:32 -0300

    :lipstick: Create website and title with CSS
    Create the website header and title with CSS. Creates a browser reset. Create the roots. Color the website background.

---
+++ src/components/HeaderTitle/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+function headerTitle() {
+    return /*html*/`
+    <header>
+        <h1 class="header-title">Pixel to Rem</h1>
+    </header>
+    
+    `
+};
+
+export default headerTitle
