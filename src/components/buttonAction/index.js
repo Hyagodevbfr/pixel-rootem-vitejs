e26599112a48905205df4cfe014858d47802e528
Hyago Brito <110845341+Hyagodevbfr@users.noreply.github.com>
2022-09-29 17:35:26 -0300

    :bug: adciona o evento de click no buttonAction.

--- src/components/buttonAction/index.js
+++ src/components/buttonAction/index.js
@@ -1,8 +1,23 @@
+import readData from '../../modules/readData'
+import validatorData from '../../modules/validatorData'
+
 import './style.css'
 
 function buttonAction(){
+    window.buttonaction = {}
+    window.buttonaction.handleClick = () => {
+        //   const numberPixel = Number(document.querySelector('#number-pixel'))
+        //   const numberRem = Number(document.querySelector('#number-rem'))
+    
+      let values = readData(numberPixel, numberRem)
+
+      if(validatorData(values)){
+        console.log(values);
+      }
+        
+    }
     return /*html*/`
-     <button class="convert-button" onClick="convertButton()">converter</button>
+     <button class="convert-button" onclick="buttonaction.handleClick()">converter</button>
     `
 };
 
---
+++ src/modules/readData.js
@@ -0,0 +1,13 @@
+   const numberPixel = Number(document.querySelector('#number-pixel')).value
+   const numberRem = Number(document.querySelector('#number-rem')).value
+
+function readData(){
+
+    let values = {}
+      numberPixel,
+      numberRem
+
+    return values
+}
+
+export default readData;
