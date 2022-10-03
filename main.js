67172db87078d125d4008b33c1832cb2fb0740dd
Hyago Brito <110845341+Hyagodevbfr@users.noreply.github.com>
2022-10-03 10:55:24 -0300

    :iphone: work on mobile version

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+import './src/tools/mediaQuery.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
+import './src/tools/mediaQuery.css'
 import headerTitle from './src/components/HeaderTitle/';
 import mainContent from './src/objects/mainContent';
+
 
 const $body = document.querySelector('#root');
 
